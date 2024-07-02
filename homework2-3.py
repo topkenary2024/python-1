508cf3b159fec0a71f1235be34a83d97fe20b17e
topkenary <topkenary@gmail.com>
2024-07-02 19:31:30 +0800

    first

---
+++ homework2-3.py
@@ -0,0 +1,4 @@
+name = "Eric"  # Replace "Eric" with the actual person's name
+message = f"Hello {name}, would you like to learn some Python today?"
+print(message)
+
---
+++ homework2-8.py
@@ -0,0 +1,4 @@
+print(4 + 4)  # Addition
+print(12 - 4)  # Subtraction
+print(4 * 2)  # Multiplication
+print(16 / 2)  # Division (notice we get 8.0 as Python performs floating-point division by default)
